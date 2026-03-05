db71794f5cb9f1240931d4e0595ce16501bda7f0
THSPSS <sunyeongbang98@gmail.com>
2026-03-05 20:33:10 +0900

    update main class

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    //this method need to run code
+    public static void main(String[] args){
+        //comment for you and your fellow developer
+        //println : change line or using \n
+        /*
+        * THIS
+        * IS
+        * MULTIPLE
+        * LINE COMENT
+        * */
+        System.out.println("Jesus Christ is The Truth & Life!");
+        System.out.println("He is My Salvation!");
+        System.out.print("He is My Rock and My Fortress!");
+        //sotu
+        System.out.println();
+    }
+}
