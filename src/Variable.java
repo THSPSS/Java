f48f40c40beae46bb671e143409d56359a414fd2
THSPSS <sunyeongbang98@gmail.com>
2026-03-06 23:02:30 +0900

    update Java variable int, double and char

---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    //primitive
+    // double
+    //char
+    // 1.declaration : data type and name of variable
+    // 2.assignment : such as set age to 20
+    public static void main(){
+        int year = 2026;
+        int quantity = 1;
+
+        double price = 19;
+        double temperature = -12.5;
+        double width = 20.2;
+
+        System.out.println("The year is " + year);
+        System.out.println("$"+price);//$19.0
+        System.out.println(width);
+    }
+
+}
