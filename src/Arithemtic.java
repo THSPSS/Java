31a6b813e68b218644749ea743113debcc6491bc
THSPSS <sunyeongbang98@gmail.com>
2026-03-09 19:39:59 +0900

    study arithmetic operation of java

---
+++ src/Arithemtic.java
@@ -0,0 +1,32 @@
+public class Arithemtic {
+    public static void main(String[] args){
+        // Arthmetic Operators
+//        int x = 10;
+//        int y = 3;
+//        int z;
+//
+//        // z = x + y
+//           z = x - y;
+//           z = x * y;
+//           z = x / y;
+//           z = x % y;
+
+        double x = 10;
+        double y = 2;
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+        // Increment and Decrement Operators
+        int z = 1;
+        z++;
+        z--;
+
+        //Order of operation [P-E-M-D-A-S]
+
+        double result = 3 + 8 / 2.0 ;
+        System.out.println(result);
+    }
+}
